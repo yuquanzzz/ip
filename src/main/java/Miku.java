86f33618b80a11d24791932beb37820522d971a9
yuquanzzz <22824928+yuquanzzz@users.noreply.github.com>
2026-01-22 21:36:44 +0800

    Add support for echo

--- src/main/java/Miku.java
+++ src/main/java/Miku.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class Miku {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Miku!");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner sc = new Scanner(System.in);
+
+        // Display welcome message
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\t「Miku-miku ni shite ageru! ♪」");
+        System.out.println("\tHatsune Miku, your personal assistant, is now online! (´꒳`)♡");
+        System.out.println("\tI've got your schedule synced and I'm ready to keep your day in perfect rhythm.");
+        System.out.println("\tWhat’s the first task on our playlist today?");
+        System.out.println("\t____________________________________________________________");
+
+        // Echos back user input until "bye" is entered
+        String input = "";
+        while (!(input = sc.nextLine()).equals("bye")) {
+            System.out.println("\t____________________________________________________________");
+            System.out.println("\t" + input);
+            System.out.println("\t____________________________________________________________");
+        }
+
+        // Display goodbye message
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________");
     }
 }
