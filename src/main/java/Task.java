c53ef15d0820da9c6cb42da7771137b110389c2d
yuquanzzz <22824928+yuquanzzz@users.noreply.github.com>
2026-01-23 03:46:25 +0800

    Refactor Task class to be abstract and add specific task types: TodoTask, DeadlineTask, and EventTask

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,4 @@
-class Task {
+abstract class Task {
     private final String description;
     private boolean isDone;
 
@@ -18,5 +18,39 @@ class Task {
     @Override
     public String toString() {
         return (isDone ? "[X] " : "[ ] ") + description;
+    }
+}
+
+class TodoTask extends Task {
+    TodoTask(String description) {
+        super(description);
+    }
+}
+
+class DeadlineTask extends Task {
+    private final String deadline;
+
+    DeadlineTask(String description, String deadline) {
+        super(description);
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " (by: " + deadline + ")";
+    }
+}
+
+class EventTask extends Task {
+    private final String eventTime;
+
+    EventTask(String description, String eventTime) {
+        super(description);
+        this.eventTime = eventTime;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " (at: " + eventTime + ")";
     }
 }
