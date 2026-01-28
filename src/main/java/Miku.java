36ff9d1801d465476c11d597a28252b2b483682b
Tan Yu Quan <22824928+yuquanzzz@users.noreply.github.com>
2026-01-28 16:28:14 +0800

    Refactor Miku class to use static Ui methods

--- src/main/java/Miku.java
+++ src/main/java/Miku.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class Miku {
-    private final Ui ui;
     private final TaskList taskList;
 
     public Miku() {
@@ -11,7 +10,7 @@ public class Miku {
 
     public void run() {
         Scanner sc = new Scanner(System.in);
-        ui.showWelcome();
+        Ui.showWelcome();
 
         boolean exit = false;
         while (!exit) {
@@ -55,7 +54,7 @@ public class Miku {
                                 "Please try again with a valid command.");
                 }
             } catch (MikuException e) {
-                ui.showError(e.getMessage());
+                Ui.showError(e.getMessage());
             }
         }
 
@@ -64,32 +63,32 @@ public class Miku {
     }
 
     private void handleList() {
-        ui.showTaskList(taskList.getTasks());
+        Ui.showTaskList(taskList.getTasks());
     }
 
     private void handleAddTask(Command command, String arguments) throws MikuException {
         Task task = Parser.parseTask(command, arguments);
         taskList.addTask(task);
-        ui.showTaskAdded(task, taskList.size());
+        Ui.showTaskAdded(task, taskList.size());
     }
 
     private void handleDeleteTask(String indexString) throws MikuException {
         int index = Parser.parseTaskIndex(indexString);
         Task task = taskList.getTask(index);
         taskList.deleteTask(index);
-        ui.showTaskDeleted(task, taskList.size());
+        Ui.showTaskDeleted(task, taskList.size());
     }
 
     private void handleMark(String indexString) throws MikuException {
         int index = Parser.parseTaskIndex(indexString);
         taskList.markTask(index);
-        ui.showTaskMarked(taskList.getTask(index));
+        Ui.showTaskMarked(taskList.getTask(index));
     }
 
     private void handleUnmark(String indexString) throws MikuException {
         int index = Parser.parseTaskIndex(indexString);
         taskList.unmarkTask(index);
-        ui.showTaskUnmarked(taskList.getTask(index));
+        Ui.showTaskUnmarked(taskList.getTask(index));
     }
 
     public static void main(String[] args) {
