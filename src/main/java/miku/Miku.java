dc640e7e6750eac0d82418b62f4822ddb27278b9
Tan Yu Quan <22824928+yuquanzzz@users.noreply.github.com>
2026-01-30 23:10:34 +0800

    Refactor Miku class to move execution logic to command classes

--- src/main/java/miku/Miku.java
+++ src/main/java/miku/Miku.java
@@ -1,97 +1,43 @@
 package miku;
 
 import miku.exception.MikuException;
-import miku.parser.Command;
+import miku.command.Command;
 import miku.parser.Parser;
 import miku.storage.Storage;
-import miku.task.Task;
 import miku.task.TaskList;
 import miku.ui.Ui;
 
 import java.io.File;
-import java.util.Scanner;
 
 public class Miku {
-    static final String STORAGE_DIR = System.getProperty("user.home") + File.separator + ".miku" + File.separator;
-    private final TaskList taskList;
+    private static final String STORAGE_DIR = System.getProperty("user.home") + File.separator 
+            + ".miku" + File.separator;     // saves user data under .miku folder
+    
+    private final Storage storage;
+    private final TaskList tasks;
+    private final Ui ui;
 
     public Miku() {
-        this.taskList = Storage.loadTaskList(STORAGE_DIR);
+        ui = new Ui();
+        storage = new Storage(STORAGE_DIR);
+        tasks = storage.loadTaskList();
     }
 
     public void run() {
-        Scanner sc = new Scanner(System.in);
-        Ui.showWelcome();
-
-        boolean exit = false;
-        while (!exit) {
+        ui.showWelcome();
+        boolean isExit = false;
+        while (!isExit) {
             try {
-                String input = sc.nextLine();
-                Command command = Parser.parseCommand(input);
-                String[] parsedInput = Parser.parseInput(input);
-
-                switch (command) {
-                case LIST:
-                    handleList();
-                    break;
-                case TODO:
-                case DEADLINE:
-                case EVENT:
-                    handleAddTask(command, parsedInput.length > 1 ? parsedInput[1] : null);
-                    break;
-                case MARK:
-                case UNMARK:
-                case DELETE:
-                    handleTaskAction(command, parsedInput.length > 1 ? parsedInput[1] : null);
-                    break;
-                case BYE:
-                    exit = true;
-                    break;
-                case UNKNOWN:
-                    throw new MikuException("I'm sorry, I didn't quite catch that command. " +
-                            "Please try again with a valid command.");
-                }
+                String fullCommand = ui.readCommand();
+                ui.showLine(); // show the divider line ("_______")
+                Command c = Parser.parse(fullCommand);
+                c.execute(tasks, ui, storage);
+                isExit = c.isExit();
             } catch (MikuException e) {
-                Ui.showError(e.getMessage());
+                ui.showError(e.getMessage());
+            } finally {
+                ui.showLine();
             }
-        }
-
-        Storage.saveTaskList(STORAGE_DIR, taskList);
-        Ui.showGoodbye();
-        sc.close();
-    }
-
-    private void handleList() {
-        Ui.showTaskList(taskList.getTasks());
-    }
-
-    private void handleAddTask(Command command, String arguments) throws MikuException {
-        Task task = Parser.parseTask(command, arguments);
-        taskList.addTask(task);
-        Storage.saveTaskList(STORAGE_DIR, taskList);
-        Ui.showTaskAdded(task, taskList.size());
-    }
-
-    private void handleTaskAction(Command command, String indexString) throws MikuException {
-        int index = Parser.parseTaskIndex(indexString);
-        Task task = taskList.getTask(index);
-
-        switch (command) {
-        case MARK:
-            taskList.markTask(index);
-            Storage.saveTaskList(STORAGE_DIR, taskList);
-            Ui.showTaskMarked(task);
-            break;
-        case UNMARK:
-            taskList.unmarkTask(index);
-            Storage.saveTaskList(STORAGE_DIR, taskList);
-            Ui.showTaskUnmarked(task);
-            break;
-        case DELETE:
-            taskList.deleteTask(index);
-            Storage.saveTaskList(STORAGE_DIR, taskList);
-            Ui.showTaskDeleted(task, taskList.size());
-            break;
         }
     }
 
