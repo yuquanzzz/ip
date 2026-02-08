6982d527ee23fb7fdc829ff130c3f2c85d93156f
Tan Yu Quan <22824928+yuquanzzz@users.noreply.github.com>
2026-02-09 04:54:27 +0800

    Remove unused stream import in TaskList.java

--- src/main/java/miku/task/TaskList.java
+++ src/main/java/miku/task/TaskList.java
@@ -5,7 +5,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import miku.exception.MikuException;
 
